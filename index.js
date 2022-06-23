800dc38f6fbee83bba14452188cf52096971bda7
--Comfort Adegbe <comiluve20@gmail.com>
2022-06-23 16:12:07 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+let myName="Comfort Adegbe"; 
+let height="5.2ft";
+let country="Nigeria";
+
+document.write('Name: ' + myName + "<br>");
+document.write('Height: ' + height + "<br>");
+document.write('Country: ' + country + "<br>");
